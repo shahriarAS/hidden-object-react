d99b74c68e32ce82e9b0af0c7203724ab275dddd
Shahriar Ahmed Shovon <shovonshahriar01@gmail.com>
2022-07-15 01:26:49 +0600

    V2 init

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import GameScreen from "./pages/GameScreen"
+import wood from "./assets/images/wood_black.jpg";
+import GameScreen from "./pages/GameScreen";
 
 function App() {
 
   return (
-    <>
+    <div style={{ backgroundImage: `url(${wood})` }}>
       <GameScreen />
-    </>
+    </div>
   )
 }
 
