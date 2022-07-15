6c96a5f4f4a24564b77ec1899a9c8c2f6e96dd0e
Shahriar Ahmed Shovon <shovonshahriar01@gmail.com>
2022-07-15 15:21:44 +0600

    Sizing Fixed On F11

--- src/pages/GameScreen.jsx
+++ src/pages/GameScreen.jsx
@@ -4,7 +4,7 @@ import GameOptions from "../components/game/GameOptions";
 function GameScreen() {
     return (
         <>
-            <div className="game-screen w-3/4 m-auto flex items-center justify-center">
+            <div className="game-screen h-screen m-auto flex items-center justify-center">
                 <GameArea />
                 <GameOptions />
             </div>
