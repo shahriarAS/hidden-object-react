b201c403fb7d2f68f454ec9ec1196e574e890d90
Shahriar Ahmed Shovon <shovonshahriar01@gmail.com>
2022-07-16 18:27:48 +0600

    Added Vanish Animation

--- src/components/game/GameArea.jsx
+++ src/components/game/GameArea.jsx
@@ -1,25 +1,37 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import toast from 'react-hot-toast';
 import bg1 from "../../assets/images/bg1.jpg";
+import vanish from "../../assets/images/vanish.gif";
 import useStore from "../../store";
 import getFilename from "../../utils/getFilename";
 import HiddenObject from "./HiddenObject";
 
 function GameArea() {
     const [imgItemState, setImgItemState] = useState([]);
+    const [performBlast, setPerformBlast] = useState(false);
+    const vanishRef = useRef()
     const state = useStore((state) => state)
 
     const removeItem = (e, item) => {
         const soln = state.targetItems[`level${state.level}`].map(i => i.file)
         if (soln.includes(getFilename(item))) {
+            const vanishEl = vanishRef.current
+            vanishEl.style.left = `${e.target.x - (e.target.width + 20)}px`
+            vanishEl.style.top = `${e.target.y - (e.target.height + 20)}px`
+            vanishEl.classList.remove("hidden")
+            e.target.classList.add("bounce-out-top")
+
             toast.success('Correct! Got 1 Point.')
+
             setTimeout(function () {
                 e.target.style.visibility = "hidden"
                 e.target.style.opacity = 0
                 state.removeTargetItem(state.level, getFilename(item))
                 state.addScore()
-            }, 500);
-            e.target.classList.add("bounce-out-top")
+                setTimeout(function () {
+                    vanishEl.classList.add("hidden")
+                }, 400);
+            }, 400);
         } else {
             toast.error("Wrong! 10 Seconds Reduced.")
             setTimeout(function () {
@@ -63,7 +75,8 @@ function GameArea() {
 
     return (
         <div className="game-area w-full h-full border-2 mr-2 relative overflow-hidden bg-contain bg-no-repeat">
-            <img src={bg1} alt="" className="w-full h-full" />
+            <img src={bg1} alt="" className="w-full h-full" loading="lazy" />
+            <img src={vanish} ref={vanishRef} alt="Vanish" className="absolute hidden z-50" />
             {
                 imgItemState.map(
                     imgItem => (
--- src/components/game/HiddenObject.jsx
+++ src/components/game/HiddenObject.jsx
@@ -2,7 +2,9 @@ import getFilename from "../../utils/getFilename";
 
 function HiddenObject({ img, top, left, removeItem }) {
     return (
-        <img src={img} width={60} height={40} className="absolute" alt={getFilename(img)} style={{ left: `${left}%`, top: `${top}%`, visibility: "visible", opacity: 1, transition: "opacity 250ms ease-in, visibility 0ms ease-in 250ms" }} onClick={(e) => removeItem(e, img)} />
+        <>
+            <img src={img} width={60} height={40} className="absolute" alt={getFilename(img)} style={{ left: `${left}%`, top: `${top}%`, visibility: "visible", opacity: 1, transition: "opacity 250ms ease-in, visibility 0ms ease-in 250ms" }} onClick={(e) => removeItem(e, img)} loading="lazy" />
+        </>
     );
 }
 
