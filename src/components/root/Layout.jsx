fcb458dfdfb24ccc917c50de5d9df7c8cc4f4ec7
shahriarAS <shovonshahriar01@gmail.com>
2025-07-03 16:56:07 +0600

    fix: height screen

--- src/components/root/Layout.jsx
+++ src/components/root/Layout.jsx
@@ -2,13 +2,13 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 function Layout({ childComp }) {
-    return (
-        <div className="max-w-[1440px] w-full flex flex-col items-center justify-between m-auto h-full">
-            <Navbar />
-            {childComp}
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="max-w-[1440px] w-full flex flex-col items-center justify-between m-auto min-h-screen h-full">
+      <Navbar />
+      {childComp}
+      <Footer />
+    </div>
+  );
 }
 
 export default Layout;
